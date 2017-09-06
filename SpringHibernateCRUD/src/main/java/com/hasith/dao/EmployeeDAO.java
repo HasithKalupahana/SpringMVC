eaf3dd780870c21fcf3659d5dd78a2b93860e2ef
Randika Isuru <randikaisuru@gmail.com>
2017-09-06 19:13:49 +0530

    initial commit

---
+++ SpringHibernateCRUD/src/main/java/com/hasith/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package com.hasith.dao;
+
+import java.util.List;
+
+import com.hasith.model.Employee;
+
+public interface EmployeeDAO {
+	
+	public void addEmployee(Employee employee);
+
+	public List<Employee> getAllEmployees();
+
+	public void deleteEmployee(Integer employeeId);
+
+	public Employee updateEmployee(Employee employee);
+
+	public Employee getEmployee(int employeeid);
+
+}
